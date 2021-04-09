9bfcf944c9410aab888b1651faa7826c2d0933e3
aleksyprok <aleksyprok@gmail.com>
2021-04-09 17:25:00 +0100

    a

--- hexaRelax2/Python/read_data.py
+++ hexaRelax2/Python/read_data.py
@@ -29,54 +29,113 @@ bby_a = (aax[1:, :,  :] - aax[:-1, :,   :]) / dz \
 bbz_a = (aay[:,  :, 1:] - aay[0,   :, :-1]) / dx \
       - (aax[:, 1:,  :] - aax[0, :-1,   :]) / dy
 
+ccx = (bbz[:, 1:, :] - bbz[:, :-1, :]) / dy \
+    - (bby[1:, :, :] - bby[:-1, :, :]) / dz
+ccy = (bbx[1:, :, :] - bbx[:-1, :, :]) / dz \
+    - (bbz[:, :, 1:] - bbz[:, :, :-1]) / dx
+ccz = (bby[:, :, 1:] - bby[:, :, :-1]) / dx \
+    - (bbx[:, 1:, :] - bbx[:, :-1, :]) / dy
+
+divb  = (bbx[1:-1, 1:-1, 1:] - bbx[1:-1, 1:-1, :-1]) / dx
+divb += (bby[1:-1, 1:, 1:-1] - bby[1:-1, :-1, 1:-1]) / dy
+divb += (bbz[1:, 1:-1, 1:-1] - bbz[:-1, 1:-1, 1:-1]) / dz
+
 fig = plt.figure()
 fig_size = fig.get_size_inches()
 fig_size[0] = fig_size[0] * 3
-fig_size[1] = fig_size[1] * 3
+fig_size[1] = fig_size[1] * 4
 fig.set_size_inches(fig_size)
 
-ax = fig.add_subplot(331)
-im = ax.imshow(bbx[1, :, :], origin='lower')
+ax = fig.add_subplot(341)
+im = ax.imshow(ccx[0, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(332)
-im = ax.imshow(bby[1, :, :], origin='lower')
+ax = fig.add_subplot(342)
+im = ax.imshow(ccy[0, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(333)
-im = ax.imshow(bbz[0, :, :], origin='lower')
+ax = fig.add_subplot(343)
+im = ax.imshow(ccz[0, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(334)
-im = ax.imshow(bbx_a[0, :, :], origin='lower')
+ax = fig.add_subplot(344)
+im = ax.imshow(divb[0, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(335)
-im = ax.imshow(bby_a[0, :, :], origin='lower')
+ax = fig.add_subplot(345)
+im = ax.imshow(ccx[1, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(336)
-im = ax.imshow(bbz_a[0, :, :], origin='lower')
+ax = fig.add_subplot(346)
+im = ax.imshow(ccy[1, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(337)
-im = ax.imshow(bbx[1, 1:-1, :] - bbx_a[0, :, :], origin='lower')
+ax = fig.add_subplot(347)
+im = ax.imshow(ccz[1, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(338)
-im = ax.imshow(bby[1, :, 1:-1] - bby_a[0, :, :], origin='lower')
+ax = fig.add_subplot(348)
+im = ax.imshow(divb[0, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-ax = fig.add_subplot(339)
-im = ax.imshow(bbz[0, 1:-1, 1:-1] - bbz_a[0, :, :], origin='lower')
+ax = fig.add_subplot(349)
+im = ax.imshow(ccx[2, :, :], origin='lower')
 cb = fig.colorbar(im)
 
-print(bbx[1, 1:-1, :] - bbx_a[0, :, :])
-print(bby[1, :, 1:-1] - bby_a[0, :, :])
-print(bbz[0, 1:-1, 1:-1] - bbz_a[0, :, :])
+ax = fig.add_subplot(3, 4, 10)
+im = ax.imshow(ccy[2, :, :], origin='lower')
+cb = fig.colorbar(im)
 
-print(np.amax(np.abs(bbx[1, 1:-1, :] - bbx_a[0, :, :])))
-print(np.amax(np.abs(bby[1, :, 1:-1] - bby_a[0, :, :])))
-print(np.amax(np.abs(bbz[0, 1:-1, 1:-1] - bbz_a[0, :, :])))
+ax = fig.add_subplot(3, 4, 11)
+im = ax.imshow(ccz[2, :, :], origin='lower')
+cb = fig.colorbar(im)
+
+ax = fig.add_subplot(3, 4, 12)
+im = ax.imshow(divb[2, :, :], origin='lower')
+cb = fig.colorbar(im)
+
+# ax = fig.add_subplot(331)
+# im = ax.imshow(bbx[1, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(332)
+# im = ax.imshow(bby[1, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(333)
+# im = ax.imshow(bbz[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(334)
+# im = ax.imshow(bbx_a[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(335)
+# im = ax.imshow(bby_a[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(336)
+# im = ax.imshow(bbz_a[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(337)
+# im = ax.imshow(bbx[1, 1:-1, :] - bbx_a[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(338)
+# im = ax.imshow(bby[1, :, 1:-1] - bby_a[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+#
+# ax = fig.add_subplot(339)
+# im = ax.imshow(bbz[0, 1:-1, 1:-1] - bbz_a[0, :, :], origin='lower')
+# cb = fig.colorbar(im)
+
+# print(bbx[1, 1:-1, :] - bbx_a[0, :, :])
+# print(bby[1, :, 1:-1] - bby_a[0, :, :])
+# print(bbz[0, 1:-1, 1:-1] - bbz_a[0, :, :])
+#
+# print(np.amax(np.abs(bbx[1, 1:-1, :] - bbx_a[0, :, :])))
+# print(np.amax(np.abs(bby[1, :, 1:-1] - bby_a[0, :, :])))
+# print(np.amax(np.abs(bbz[0, 1:-1, 1:-1] - bbz_a[0, :, :])))
 
 plt.show()
